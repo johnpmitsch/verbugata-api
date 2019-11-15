342be51457a1b3f888763e54b8acb7d1756c3956
John Mitsch <jomitsch@redhat.com>
2019-11-15 17:04:19 -0500

    Updating db path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json', { foreignKeySuffix: '_id' });
+const db = fs.readFileSync(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(db, { foreignKeySuffix: '_id' });
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
