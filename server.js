11bd28bac71be04882cd5ecf8c9f5c763314a286
John Mitsch <jomitsch@redhat.com>
2019-11-27 00:05:47 -0500

    Make read only

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const db = path.join(__dirname, 'db.json')
-const router = jsonServer.router(db, { foreignKeySuffix: '_id' });
+const router = jsonServer.router(db, { foreignKeySuffix: '_id', readOnly: true });
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
