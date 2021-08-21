4dc57d24ebd573fae476883d0e0017f59f598cb0
John Mitsch <jmitsch@minervaproject.com>
2021-08-21 18:28:59 -0400

    added conjugations

--- to_csv.js
+++ to_csv.js
@@ -6,6 +6,7 @@ const language_id = 1;
 
 const tenses_csv_path = "./csvs/tenses.csv";
 const verbs_csv_path = "./csvs/verbs.csv";
+const conjugations_csv_path = "./csvs/conjugations.csv";
 
 // Create a CSV from data that can be used to seed a database
 
@@ -48,4 +49,58 @@ fs.truncate(verbs_csv_path, 0, () => {
     fs.appendFileSync(verbs_csv_path, row);
   });
   console.log(`${verbs_csv_path} written!`);
+});
+
+/* CONJUGATIONS TABLE */
+
+const buildConjugationsRow = (i, verb_id, tense_id, pronoun, finite) => {
+  return `${i},${verb_id},${tense_id},${pronoun},${finite}\n`;
+};
+
+fs.truncate(conjugations_csv_path, 0, () => {
+  console.log("\nWRITING CONJUGATIONS\n");
+  console.log(`${conjugations_csv_path} cleared`);
+  const header = "id,verb_id,tense_id,pronoun,finite\n";
+  fs.appendFileSync(conjugations_csv_path, header);
+  const conjugations = data["conjugations"];
+  let counter = 0;
+  console.log(
+    `Writing ${conjugations.length} conjugation groups to ${conjugations_csv_path}`
+  );
+  conjugations.forEach((conjugation, i) => {
+    const { verb_id } = conjugation; // will need to rethink this for more languages, verb_id is just an index/rank now
+
+    Object.keys(conjugation).forEach((conjTables) => {
+      if (conjTables != "verb_id") {
+        const tense_id = tenses.indexOf(conjTables) + 1;
+        const conjTable = conjugation[conjTables];
+
+        // some tenses are just one conjugation
+        if (typeof conjTable === "string") {
+          counter += 1;
+          const row = buildConjugationsRow(
+            counter,
+            verb_id,
+            tense_id,
+            null,
+            conjTable
+          );
+          fs.appendFileSync(conjugations_csv_path, row);
+        } else {
+          Object.entries(conjTable).forEach(([pronoun, finite]) => {
+            counter += 1;
+            const row = buildConjugationsRow(
+              counter,
+              verb_id,
+              tense_id,
+              pronoun,
+              finite
+            );
+            fs.appendFileSync(conjugations_csv_path, row);
+          });
+        }
+      }
+    });
+  });
+  console.log(`${conjugations_csv_path} written!`);
 });
