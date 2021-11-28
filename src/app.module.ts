fad22eefd17aadcee42e41203e32512de4449e08
John Mitsch <jmitsch@minervaproject.com>
2021-11-28 10:24:52 -0500

    Added login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { VerbsModule } from './verbs/verbs.module';
+import { VerbsModule } from "./verbs/verbs.module";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [VerbsModule],
+  imports: [VerbsModule, ConfigModule.forRoot(), AuthModule, UsersModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./local.strategy";
+import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { jwtConstants } from "./constants";
+import { JwtStrategy } from "./jwt.strategy";
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "14d" },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
