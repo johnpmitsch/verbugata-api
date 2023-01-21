336e2d924a0fe733e115fae466e3e8c66bbc41c1
John Mitsch <john@quicknode.com>
2023-01-21 09:25:51 -0500

    GQL server starting

---
+++ index.ts
@@ -0,0 +1,51 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Verb {
+    infinitive: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    verbs: [Verb]
+  }
+`;
+
+const Verbs = [
+  {
+    infinitive: "ser",
+  },
+  {
+    infinitive: "estar",
+  },
+];
+
+const resolvers = {
+  Query: {
+    verbs: () => Verbs,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const startServer = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+startServer();
